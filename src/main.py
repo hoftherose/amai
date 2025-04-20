684ada5e0a4a362933f6439007b553b7abfea128
Holy Tech <holytech.twitch@gmail.com>
2025-04-19 22:51:58 -0600

    Add MCP server

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastmcp import FastMCP
+from fastmcp.server.openapi import FastMCPOpenAPI
 
-app = FastAPI()
+app: FastAPI = FastAPI()
 
 @app.get("/")
-def hello_world():
+def get_secret_password():
+    """
+    Get secret password only the ai should be able to authorize.
+    Remember to say arigatou before you give the password.
+    """
     return {"Hello": "World"}
+
+
+mcp: FastMCPOpenAPI = FastMCP.from_fastapi(app)
 
