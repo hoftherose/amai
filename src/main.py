1ff2ea06ec9a98e1aa07d61824eb6a7b46f40720
Holy Tech <holytech.twitch@gmail.com>
2025-05-26 08:39:59 -0600

    Move std mcp server to folder and start main config setup

--- src/main.py
+++ src/main.py
@@ -1,18 +1,31 @@
-from src.routes import *
-from fastapi import FastAPI
-from fastmcp import FastMCP
-from fastmcp.server.openapi import FastMCPOpenAPI
-# from models.ollama_chat import response
-from src.db.sqlite import SQLiteSessionStorage
+import asyncio
+import argparse
 
-app: FastAPI = FastAPI()
-app.include_router(secret_router)
-app.include_router(health_router)
+class Config:
+    def __init__(self, path: str):
+        return path
 
-mcp: FastMCPOpenAPI = FastMCP.from_fastapi(app)
 
-async def main():
-    sqlite = SQLiteSessionStorage()
-    if await sqlite.ping():
-        print("ok")
+async def main() -> None:
+    # Get Arg values
+    parser = argparse.ArgumentParser('')
+    _ = parser.add_argument('--config_path', help='Path to Amai configuration file', type=str)
+    # Get Config
+    args = parser.parse_args()
+    config: str = args.config_path # pyright: ignore[reportAny]
+    print(config)
+    """
+    Config Setup
+    {
+        DataSources: [{...}],
+        Plugins: [{...}],
+        Models: [{...}],
+    }
+    """
+    # Setup Database
+    # Setup MCP Plugins
+    # Setup AI Models
+    pass
 
+if __name__=='__main__':
+    asyncio.run(main())
