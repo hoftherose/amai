e0ba63e80e291794e659977885016ee5da0357a2
Holy Tech <holytech.twitch@gmail.com>
2025-07-10 23:48:44 -0600

    Revert main file

--- src/main.py
+++ src/main.py
@@ -1,31 +1,18 @@
-import asyncio
-import argparse
+from src.routes import *
+from fastapi import FastAPI
+from fastmcp import FastMCP
+from fastmcp.server.openapi import FastMCPOpenAPI
+# from models.ollama_chat import response
+from src.db.sqlite import SQLiteSessionStorage
 
-class Config:
-    def __init__(self, path: str):
-        return path
+app: FastAPI = FastAPI()
+app.include_router(secret_router)
+app.include_router(health_router)
 
+mcp: FastMCPOpenAPI = FastMCP.from_fastapi(app)
 
-async def main() -> None:
-    # Get Arg values
-    parser = argparse.ArgumentParser('')
-    _ = parser.add_argument('--config_path', help='Path to Amai configuration file', type=str)
-    # Get Config
-    args = parser.parse_args()
-    config: str = args.config_path # pyright: ignore[reportAny]
-    print(config)
-    """
-    Config Setup
-    {
-        DataSources: [{...}],
-        Plugins: [{...}],
-        Models: [{...}],
-    }
-    """
-    # Setup Database
-    # Setup MCP Plugins
-    # Setup AI Models
-    pass
+async def main():
+    sqlite = SQLiteSessionStorage()
+    if await sqlite.ping():
+        print("ok")
 
-if __name__=='__main__':
-    asyncio.run(main())
