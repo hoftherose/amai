a4dfc8731ac85fa752e62b11b0d5feb8dd2ae3a4
Holy Tech <holytech.twitch@gmail.com>
2025-07-12 12:56:14 -0600

    Move notmain back and import app

--- src/main.py
+++ src/main.py
@@ -1,18 +1,40 @@
-from src.routes import *
-from fastapi import FastAPI
-from fastmcp import FastMCP
-from fastmcp.server.openapi import FastMCPOpenAPI
-# from models.ollama_chat import response
-from src.db.sqlite import SQLiteSessionStorage
+import asyncio
+import argparse
+from .api import app # pyright: ignore[reportUnusedImport]
+from .db import DataSources
 
-app: FastAPI = FastAPI()
-app.include_router(secret_router)
-app.include_router(health_router)
+class Config:
+    def __init__(self, path: str):
+        with open(path, 'r') as f:
+            config = Config(f.read())
+            return config
 
-mcp: FastMCPOpenAPI = FastMCP.from_fastapi(app)
+    def get_datasources(self) -> list[DataSources]:
+        return []
 
-async def main():
-    sqlite = SQLiteSessionStorage()
-    if await sqlite.ping():
-        print("ok")
 
+async def main() -> None:
+    # Get Arg values
+    parser = argparse.ArgumentParser('')
+    _ = parser.add_argument('--config_path', help='Path to Amai configuration file', type=str)
+    # Get Config
+    args = parser.parse_args()
+    config = Config(args.config_path) # pyright: ignore[reportAny]
+    print(config)
+    """
+    Config Setup
+    {
+        DataSources: [{...}],
+        Plugins: [{...}],
+        Models: [{...}],
+    }
+    """
+    # Setup Database
+    for sources in config.get_datasources():
+        sources.setup()
+    # Setup MCP Plugins
+    # Setup AI Models
+    pass
+
+if __name__=='__main__':
+    asyncio.run(main())
