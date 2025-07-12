f294940fcaf8da14825df70ed8660d58c57a424c
Holy Tech <holytech.twitch@gmail.com>
2025-07-12 13:09:25 -0600

    Add basic lifespan for application

--- src/main.py
+++ src/main.py
@@ -1,19 +1,18 @@
-import asyncio
 import argparse
-from .api import app # pyright: ignore[reportUnusedImport]
+from typing import override
+from contextlib import asynccontextmanager
+
 from .db import DataSources
 
-class Config:
-    def __init__(self, path: str):
-        with open(path, 'r') as f:
-            config = Config(f.read())
-            return config
+from src.routes import *
+from fastapi import FastAPI
+from fastmcp import FastMCP
+from fastmcp.server.openapi import FastMCPOpenAPI
+# from models.ollama_chat import response
+# from src.db.sqlite import SQLiteSessionStorage
 
-    def get_datasources(self) -> list[DataSources]:
-        return []
-
-
-async def main() -> None:
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
     # Get Arg values
     parser = argparse.ArgumentParser('')
     _ = parser.add_argument('--config_path', help='Path to Amai configuration file', type=str)
@@ -34,7 +33,34 @@ async def main() -> None:
         sources.setup()
     # Setup MCP Plugins
     # Setup AI Models
-    pass
+    yield
+    # Teardown Database
+    for sources in config.get_datasources():
+        sources.shutdown()
+    # Teardown MCP Plugins
+    # Teardown AI Models
 
-if __name__=='__main__':
-    asyncio.run(main())
+app: FastAPI = FastAPI(lifespan=lifespan)
+app.include_router(secret_router)
+app.include_router(health_router)
+
+mcp: FastMCPOpenAPI = FastMCP.from_fastapi(app)
+
+# async def main():
+#     sqlite = SQLiteSessionStorage()
+#     if await sqlite.ping():
+#         print("ok")
+
+class Config:
+    def __init__(self, path: str):
+        with open(path, 'r') as f:
+            self.config: str = f.read()
+
+    @override
+    def __repr__(self) -> str:
+        return self.config
+
+    def get_datasources(self) -> list[DataSources]:
+        return []
+
+
